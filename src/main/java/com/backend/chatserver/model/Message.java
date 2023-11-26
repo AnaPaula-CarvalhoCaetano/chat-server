5b4645627187d4529fe2780e37d421765eb3f345
AnaPaula-CarvalhoCaetano <paula.youssef@hotmail.com>
2023-11-26 19:43:18 -0300

    Atualizações na model

--- src/main/java/com/backend/chatserver/model/Message.java
+++ src/main/java/com/backend/chatserver/model/Message.java
@@ -1,36 +1,38 @@
 package com.backend.chatserver.model;
 
 import com.backend.chatserver.Enum.Status;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
-public class Message {
-	
-	private String senderName;
-	private String receiverName;
-	private String message;
-	private String date;
-	private Status status;
-	
-	
-	public Message() {
-	}
-	
-	
-	public Message(String senderName, String receiverName, String message, String date, Status status) {
-		super();
-		this.senderName = senderName;
-		this.receiverName = receiverName;
-		this.message = message;
-		this.date = date;
-		this.status = status;
-	}
-	
-	
 
+@Document(collection = "messages")
+public class Message {
+
+    @Id
+    private String id;
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private LocalDateTime date; 
+    private Status status;
+
+    public Message() {
+        this.date = LocalDateTime.now();
+    }
+
+    public Message(String senderName, String receiverName, String message, Status status) {
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.message = message;
+        this.status = status;
+        this.date = LocalDateTime.now();
+    }
 }
